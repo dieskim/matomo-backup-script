a12db7c519597b3eb2399510a747728e58bc1063
Dieskim <dieskim@julula.com>
2022-08-26 10:02:30 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# Matomo Server Backup Script
+
+Matomo is the leading Free/Libre open analytics platform.
+
+Matomo is a full-featured PHP MySQL software program that you download and install on your own webserver. At the end of the five-minute installation process, you will be given a JavaScript code. Simply copy and paste this tag on websites you wish to track and access your analytics reports in real-time.
+
+Matomo aims to be a Free software alternative to Google Analytics and is already used on more than 1,400,000 websites. Privacy is built-in!
+
+Matomo:
+
+- [Matomo (Matomo)](https://matomo.org/)
+
+Matomo Github;
+
+- [Matomo Github (Matomo Github)](https://github.com/matomo-org/matomo)
+
+# This is a Linux Backup Script to Back up the Matomo Root Folder and Databases
+# It supports both Local and Remote Backups as well as backup Rotation
+# Please note that this is still a beta script
+# Any pull requests and suggestion welcome!
+# Author: Dieskim
+
+## Installation
+
+1. Log in to you server via SSH as root
+
+2. Create a folder where you would like to place the script and move into the folder
+
+``mkdir matomo-backup-script``
+
+``cd matomo-backup-script``
+
+3. wget the backup script
+
+``wget https://github.com/dieskim/matomo-backup-script/blob/master/matomo-backup.sh``
+
+4. Set the script to be executable
+
+``chmod a+x matomo-backup.sh``
+
+5. For remote backups set up [Linux SSH KEY Password Free Automatic Config Authentication](http:/dieskim.me/2015/05/13/how-to-linux-ssh-key-password-free-automatic-config-authentication-backup-sftp-scp/)
+
+6. Edit all the setting in the top of the script.
+
+7. Test the script via
+``sh -x matomo-backup.sh 2>&1 | tee debug.txt``
+
+7. Add Script to Crontab to run daily
+
+``crontab -e``
+
+- Choose your editor and add the below to the crontab to run script every night at midnight - save file
+	
+``00 00 * * * /path/to/matomo-backup-script/matomo-backup.sh``
+
+## Author
+
+Author: Dieskim
+
+## License
+MIT
---
+++ matomo-backup.sh
@@ -0,0 +1,111 @@
+#!/bin/sh
+
+## RUN USING 'sh -x matomo-backup.sh 2>&1 | tee debug.txt' to get DEBUG OUTPUT AND LOG
+
+##############################################################################################################################
+##                                          PLEASE SET ALL THE OPTIONS BELOW                                                ##
+##                      PLEASE SET UP SSH KEYS WITH CONFIG BEFORE ATTEMPTING REMOTE BACKUP                                  ##
+##  http://dieskim.me/2015/05/13/how-to-linux-ssh-key-password-free-automatic-config-authentication-backup-sftp-scp/
+
+## REQUIREMENTS
+#1. Install Matomo ExtraTools Plugin -> https://github.com/dieskim/extratools
+#2. Make sure to run -> composer require symfony/yaml:~2.6.0 / composer require symfony/process:^3.4
+
+
+## SET REMOTE BACKUP TRUE OR FALSE - DEFAULTS TO LOCALBACKUP
+REMOTEBACKUP=false
+
+## SET REMOTESERVER INFO IF USING REMOTE BACKUP - REMEMBER TO FIRST ENABLE SSH KEYS AS IN THE GUIDE ABOVE
+REMOTESERVER=backup-server-nickname
+
+## SET REMOVEDEST
+REMOTEDEST=/home/backup-user/matomo-backup
+
+## SET LOCAL DEST - SCRIPT DEFAULTS TO MOVING TAR TO LOCAL DESTINATION
+LOCALDEST=/home/yourdest/matomo-backup/
+
+## ROTATE BACKUP TRUE OR FALSE
+ROTATEBACKUP=false
+
+## SET KEEPDAYS IF USING ROTATEBACKUP - X AMOUNT OF DAYS BEFORE ROTATING
+KEEPDAYS=7
+
+## SET COUNLY BACKUP SCRIPT ROOT - BACKUP LOG CREATED HERE
+BACKUPROOT=/var/matomo-backup
+
+## SET BACKUP DIR/TAR NAME
+BACKUPDIRVAR=hostname-matomo-backup
+
+## SET MATOMO DIR
+MATOMODIR=/var/matomo
+
+##############################################################################################################################
+##                              DO NOT EDIT FROM HERE - EXCEPT IF YOU KNOW WHAT YOU ARE DOING                               ##
+
+## SET DATE 
+DATE=`date +%Y-%m-%d`
+
+## START IF ROTATEBACKUP
+if $ROTATEBACKUP
+then
+	BACKUPDIR=$BACKUPDIRVAR-$DATE
+else
+	BACKUPDIR=$BACKUPDIRVAR
+fi
+## END IF ROTATEBACKUP
+
+## CREATE BACKUPDIR
+mkdir -p $BACKUPROOT
+
+## CHANGE TO DIR
+cd $BACKUPROOT
+
+## TAR MATOMO DIR
+tar -cf $BACKUPROOT/matomo_dir.tar $MATOMODIR
+
+## SET MATOMO EXTRA TOOLS DB BACKUP PATH
+$MATOMODIR/console config:set 'ExtraTools.db_backup_path="$BACKUPROOT"'
+
+## RUN MATOMO DATABSE BACKUP
+$MATOMODIR/console database:backup --backup-path=$BACKUPROOT
+
+## TAR THE WHOLE BACKUP DIR AND REMOVE DIR IF SUCCESSFUL
+tar -cf $BACKUPROOT/$BACKUPDIR.tar $BACKUPROOT/$BACKUPDIR && rm -R $BACKUPROOT/$BACKUPDIR
+
+## START IF REMOTEBACKUP ELSE LOCALBACKUP
+if $REMOTEBACKUP;
+then
+## SEND TAR TO REMOTE SERVER
+sftp $REMOTESERVER <<_EOF_
+    put $BACKUPROOT/$BACKUPDIR.tar $REMOTEDEST/$BACKUPDIR.tar
+    bye
+_EOF_
+
+    ## START IF ROTATEBACKUP REMOVE OLD
+    if $ROTATEBACKUP
+    then
+ssh -T $REMOTESERVER <<_EOF_
+    find $REMOTEDEST/$BACKUPDIRVAR-* -mtime +$KEEPDAYS -exec rm -v {} \; | tee -a $REMOTEDEST/matomo-backup.log
+    exit
+_EOF_
+    fi
+    ## END IF ROTATEBACKUP REMOVE OLD
+
+    ## REMOVE LOCAL TAR
+    rm -v $BACKUPROOT/$BACKUPDIR.tar | tee -a $BACKUPROOT/matomo-backup.log
+else
+
+    ## MAKE LOCALDEST
+    mkdir -p $LOCALDEST
+
+    ## MOVE TAR TO LOCALDEST
+    mv -v $BACKUPROOT/$BACKUPDIR.tar $LOCALDEST | tee -a $BACKUPROOT/matomo-backup.log
+
+    ## START IF ROTATEBACKUP REMOVE OLD
+    if $ROTATEBACKUP
+    then
+        find $LOCALDEST/$BACKUPDIRVAR-* -mtime +$KEEPDAYS -exec rm -v {} \; | tee -a $BACKUPROOT/matomo-backup.log
+    fi
+    ## END IF ROTATEBACKUP REMOVE OLD
+fi
+## END IF REMOTEBACKUP ELSE LOCALBACKUP
